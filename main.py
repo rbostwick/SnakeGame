b5884f0f216c9ee5a4ac155e954a1aa7c6c1496f
Ryan Bostwick <rbostwick@students.email.com>
2022-12-11 12:36:51 -0600

    Classes and import statements

---
+++ main.py
@@ -0,0 +1,50 @@
+import pygame
+import random
+from enum import Enum
+from collections import namedtuple
+
+pygame.init()
+
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+
+Point = namedtuple('Point', 'x, y')
+
+BLOCK_SIZE = 20
+
+
+class SnakeGame:
+    def __init__(self, w=640, h=480):
+        self.w = w
+        self.h = h
+
+        # init display
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption('Snake')
+        self.clock = pygame.time.Clock()
+
+        # init game state
+        self.direction = Direction.RIGHT
+
+        self.head = Point(self.w/2, self.h/2)
+        self.snake = [self.head,
+                      Point(self.head.x-BLOCK_SIZE, self.head.y),
+                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+
+    def play_step(self):
+        pass
+
+
+if __name__ == '__main__':
+    game = SnakeGame()
+
+    # game loop
+    while True:
+        game.play_step()
+
+    pygame.quit()
